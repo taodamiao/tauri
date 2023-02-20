294928b1280f52b73c8878f27a136875f6d848a9
菜鸟不会飞 <aodamiao@foxmail.com>
2023-02-20 14:46:27 +0800

    fix

--- core/tauri/src/window.rs
+++ core/tauri/src/window.rs
@@ -1322,7 +1322,29 @@ impl<R: Runtime> Window<R> {
       "Scope not defined for window `{}` and URL `{current_url}`",
       self.window.label
     );
-    let scope = Some(&default_scope);
+    let scope = if is_local {
+      Some(&default_scope)
+    } else {
+      let mut scope = None;
+      let mut found_scope_for_window = false;
+      let mut found_scope_for_url = false;
+      for s in &manager.inner.external_command_access {
+        let matches_window = s.windows.contains(&self.window.label);
+        let matches_url = s.url.matches(current_url.as_str());
+        found_scope_for_window = found_scope_for_window || matches_window;
+        found_scope_for_url = found_scope_for_url || matches_url;
+        if matches_window && matches_url && scope.is_none() {
+          scope.replace(s);
+        }
+      }
+      if found_scope_for_window {
+        scope_not_found_error_message = format!("Scope not defined for URL `{current_url}`");
+      } else if found_scope_for_url {
+        scope_not_found_error_message =
+          format!("Scope not defined for window `{}`", self.window.label);
+      }
+      scope
+    };
     match payload.cmd.as_str() {
       "__initialized" => {
         let payload: PageLoadPayload = serde_json::from_value(payload.inner)?;
@@ -1337,11 +1359,6 @@ impl<R: Runtime> Window<R> {
         );
         let resolver = InvokeResolver::new(self, payload.callback, payload.error);
         let invoke = Invoke { message, resolver };
-
-        if !is_local && scope.is_none() {
-          invoke.resolver.reject(scope_not_found_error_message);
-          return Ok(());
-        }
 
         if let Some(module) = &payload.tauri_module {
           if !is_local && scope.map(|s| s.disable_tauri_api).unwrap_or_default() {
